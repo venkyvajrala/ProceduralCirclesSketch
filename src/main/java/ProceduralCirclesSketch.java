caa7fa99670c6c43b828a461dfdac5f9b80fbd3f
venkyvajrala <vvenkateswarlu168@gmail.com>
2022-01-17 12:42:09 +0530

    formated code

--- src/main/java/ProceduralCirclesSketch.java
+++ src/main/java/ProceduralCirclesSketch.java
@@ -1,26 +1,27 @@
 import processing.core.PApplet;
 
-public class ProceduralCirclesSketch  extends  PApplet{
+public class ProceduralCirclesSketch extends PApplet {
 
     public static final int HEIGHT = 348;
     public static final int WIDTH = 640;
     public static final int Diameter = 10;
-    int ball1HorizontalPosition=0;
-    int ball2HorizontalPosition=0;
-    int ball3HorizontalPosition=0;
-    int ball4HorizontalPosition=0;
+    int ball1HorizontalPosition = 0;
+    int ball2HorizontalPosition = 0;
+    int ball3HorizontalPosition = 0;
+    int ball4HorizontalPosition = 0;
     int ball1VerticalPosition = HEIGHT / 5;
-    int ball2VerticalPosition = HEIGHT*2 / 5;
-    int ball3VerticalPosition = HEIGHT*3 / 5;
-    int ball4VerticalPosition = HEIGHT*4 / 5;
+    int ball2VerticalPosition = HEIGHT * 2 / 5;
+    int ball3VerticalPosition = HEIGHT * 3 / 5;
+    int ball4VerticalPosition = HEIGHT * 4 / 5;
+
     public static void main(String[] args) {
-        PApplet.main(new String[] { "--present", "ProceduralCirclesSketch" });
+        PApplet.main(new String[]{"--present", "ProceduralCirclesSketch"});
     }
 
     @Override
     public void settings() {
         super.settings();
-        size(WIDTH,HEIGHT);
+        size(WIDTH, HEIGHT);
     }
 
     @Override
@@ -28,23 +29,23 @@ public class ProceduralCirclesSketch  extends  PApplet{
         super.setup();
 
 
-        ellipse(ball1HorizontalPosition, ball1VerticalPosition,Diameter, Diameter);
-        ellipse(ball2HorizontalPosition,ball2VerticalPosition,Diameter, Diameter);
-        ellipse(ball3HorizontalPosition,ball3VerticalPosition,Diameter, Diameter);
-        ellipse(ball4HorizontalPosition,ball4VerticalPosition,Diameter, Diameter);
+        ellipse(ball1HorizontalPosition, ball1VerticalPosition, Diameter, Diameter);
+        ellipse(ball2HorizontalPosition, ball2VerticalPosition, Diameter, Diameter);
+        ellipse(ball3HorizontalPosition, ball3VerticalPosition, Diameter, Diameter);
+        ellipse(ball4HorizontalPosition, ball4VerticalPosition, Diameter, Diameter);
     }
 
     @Override
     public void draw() {
 
-        ball1HorizontalPosition+=1;
-        ball2HorizontalPosition+=2;
-        ball3HorizontalPosition+=3;
-        ball4HorizontalPosition+=4;
-        ellipse(ball1HorizontalPosition,ball1VerticalPosition,Diameter, Diameter);
-        ellipse(ball2HorizontalPosition,ball2VerticalPosition,Diameter, Diameter);
-        ellipse(ball3HorizontalPosition,ball3VerticalPosition,Diameter, Diameter);
-        ellipse(ball4HorizontalPosition,ball4VerticalPosition,Diameter, Diameter);
+        ball1HorizontalPosition += 1;
+        ball2HorizontalPosition += 2;
+        ball3HorizontalPosition += 3;
+        ball4HorizontalPosition += 4;
+        ellipse(ball1HorizontalPosition, ball1VerticalPosition, Diameter, Diameter);
+        ellipse(ball2HorizontalPosition, ball2VerticalPosition, Diameter, Diameter);
+        ellipse(ball3HorizontalPosition, ball3VerticalPosition, Diameter, Diameter);
+        ellipse(ball4HorizontalPosition, ball4VerticalPosition, Diameter, Diameter);
 
     }
 }
